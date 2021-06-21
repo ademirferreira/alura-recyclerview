0ce7cb338fedb7fdb51aa27f9343d4af7dabad68
Ademir Ferreira <whoisademir@gmail.com>
2021-06-21 19:18:20 -0300

    Adicionando as classes padrões

---
+++ app/src/main/java/br/com/alura/ceep/model/Nota.java
@@ -0,0 +1,21 @@
+package br.com.alura.ceep.model;
+
+public class Nota {
+
+    private final String titulo;
+    private final String descricao;
+
+    public Nota(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -3,8 +3,15 @@ package br.com.alura.ceep.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +19,14 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira Nota", "Primeira descrição"));
+
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
     }
 }
