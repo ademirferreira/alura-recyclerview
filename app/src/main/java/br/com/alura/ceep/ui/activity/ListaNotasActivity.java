778340ce78be907e9b39b7d8d22437649e8afee7
Ademir Ferreira <whoisademir@gmail.com>
2021-06-21 18:49:29 -0300

    Adicionado o layout do projeto

---
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,16 @@
+package br.com.alura.ceep.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.alura.ceep.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
