bef79974dffeb4f3f7695bbdd77c672f456dd16f
Ademir Ferreira <whoisademir@gmail.com>
2021-06-25 22:47:10 -0300

    Salvando a nota do formulário

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
@@ -1,11 +1,16 @@
 package br.com.alura.ceep.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
 
 public class FormularioNotaActivity extends AppCompatActivity {
 
@@ -20,5 +25,18 @@ public class FormularioNotaActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_formulario_nota_salva, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_formulario_nota_ic_salva){
+            EditText titulo = findViewById(R.id.formulario_nota_titulo);
+            EditText descricao = findViewById(R.id.formulario_nota_descricao);
+
+            Nota notaCriada = new Nota(titulo.getText().toString(), descricao.getText().toString());
+            new NotaDAO().insere(notaCriada);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
