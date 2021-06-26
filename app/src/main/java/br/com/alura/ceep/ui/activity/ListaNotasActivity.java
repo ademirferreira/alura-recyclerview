9ada86e1b3c5341894ee8bfda624b418f1870853
Ademir Ferreira <whoisademir@gmail.com>
2021-06-25 22:06:23 -0300

    Adicinado formulario de nota

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,6 +1,9 @@
 package br.com.alura.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,11 +22,14 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
-
-
         List<Nota> todasNotas = notasDeExemplo();
-
         configuraRecyclerView(todasNotas);
+
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(v -> {
+            Intent iniciaFormularioNota = new Intent(ListaNotasActivity.this, FormularioNotaActivity.class);
+            startActivity(iniciaFormularioNota);
+        });
     }
 
     private List<Nota> notasDeExemplo() {
