0841fd008675fc520e52c98586c23cf9a8907970
Ademir Ferreira <whoisademir@gmail.com>
2021-06-25 22:31:32 -0300

    Adicionado o botão de salvar ao menu

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
@@ -3,6 +3,7 @@ package br.com.alura.ceep.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import br.com.alura.ceep.R;
 
@@ -12,5 +13,12 @@ public class FormularioNotaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_nota);
+        setTitle("Inserir Nota");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario_nota_salva, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
